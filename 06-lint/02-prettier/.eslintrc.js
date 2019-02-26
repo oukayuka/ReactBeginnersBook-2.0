147535d383bdc00f71f3b575be9ef613f201db2d
Yuka O'oka <oukayuka@ayeate.com>
2019-02-26 23:48:17 +0900

    Add chapter 6-2

---
+++ 06-lint/02-prettier/.eslintrc.js
@@ -0,0 +1,74 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    'jest/globals': true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    '__DEV__': true
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    project: './tsconfig.json',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'jest', 'prettier', 'react', 'react-hooks'],
+  root: true,
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+    // eslint official
+    'no-console': 'warn',
+    'require-yield': 'error',
+
+    // @typescript-eslint
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    indent: 'off',
+    '@typescript-eslint/indent': ['error', 2],
+    '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+
+    // react
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['jsx', 'tsx']
+      }
+    ],
+    'react/jsx-one-expression-per-line': 'off',
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react/prop-types': 'off',
+    'react/prefer-stateless-function': 'warn',
+
+    // prettier
+    'prettier/prettier': [
+      'error', {
+        bracketSpacing: true,
+        printWidth: 80,
+        semi: true,
+        singleQuote: true,
+        trailingComma: 'all',
+        useTabs: false
+      }
+    ]
+  }
+};
---
+++ 06-lint/02-prettier/src/App.tsx
@@ -0,0 +1,32 @@
+import { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    const logoOptions = {
+      alt: 'logo',
+      className: 'App-logo',
+      src: logo,
+    };
+    const title = 'こんにちはReact';
+    const targets = ['World', 'Kanae', 'Yukina'];
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          {
+            // コメントはこう書く
+          }
+          <img alt={logoOptions.alt} {...logoOptions} />
+          {title && <p>{title}</p>}
+          {targets.map(target => (
+            <p>Hello, {target}!</p>
+          ))}
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
