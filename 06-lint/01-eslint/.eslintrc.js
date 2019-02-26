d9d147ba2b78d6a7eef4d6c10a1693ddd4da97a2
Yuka O'oka <oukayuka@ayeate.com>
2019-02-26 12:48:10 +0900

    Add chapter 6-1

---
+++ 06-lint/01-eslint/.eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    project: './tsconfig.json',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  root: true,
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    'indent': 'off',
+    '@typescript-eslint/indent': ['error', 2],
+    '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+    'eol-last': ['error', 'always'],
+    'func-style': ['error', 'expression', { allowArrowFunctions: true }],
+    'no-dupe-class-members': 'error',
+    'no-var': 'error',
+    'object-shorthand': ['error', 'always'],
+    'prefer-arrow-callback': 'error',
+    'prefer-const': 'error',
+    'prefer-spread': 'error',
+    'require-yield': 'error'
+  }
+};
