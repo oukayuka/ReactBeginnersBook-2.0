db7f18bee8c480abb9248fa3ef565696d5508765
Yuka O'oka <oukayuka@ayeate.com>
2019-02-26 12:47:02 +0900

    Add chapter 5

---
+++ 05-jsx/02-jsx/src/App.tsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    const logoOptions = {
+      alt: "logo",
+      className: "App-logo",
+      src: logo
+    };
+    const title = "こんにちはReact";
+    const targets = ["World", "Kanae", "Yukina"];
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          {
+            // コメントはこう書く
+          }
+          <img {...logoOptions} />
+          {title && <p>{title}</p>}
+          {targets.map(target => (
+            <p>Hello, {target}!</p>
+          ))}
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
