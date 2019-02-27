b8789cdfbbc9decbb8a715121c5cb0e5dec88d1f
Yuka O'oka <oukayuka@ayeate.com>
2019-02-28 00:17:32 +0900

    Add chapter 7-4

---
+++ 07-component/04-lifecycle/src/App.tsx
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { Button, Card, Icon, Statistic } from 'semantic-ui-react';
+
+import './App.css';
+
+const LIMIT = 60;
+
+interface AppState {
+  timeLeft: number;
+}
+
+class App extends Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { timeLeft: LIMIT };
+  }
+
+  reset = () => this.setState({ timeLeft: LIMIT });
+
+  tick = () => {
+    this.setState(prevState => ({ timeLeft: prevState.timeLeft - 1 }));
+  };
+
+  componentDidMount = () => {
+    this.timerId = setInterval(this.tick, 1000);
+  };
+
+  componentDidUpdate = () => {
+    const { timeLeft } = this.state;
+    if (timeLeft === 0) {
+      this.reset();
+    }
+  };
+
+  componentWillUnmount = () => {
+    clearInterval(this.timerId as NodeJS.Timer);
+  };
+
+  timerId?: NodeJS.Timer;
+
+  render() {
+    const { timeLeft } = this.state;
+
+    return (
+      <div className="container">
+        <header>
+          <h1>タイマー</h1>
+        </header>
+        <Card>
+          <Statistic className="number-board">
+            <Statistic.Label>time</Statistic.Label>
+            <Statistic.Value>{timeLeft}</Statistic.Value>
+          </Statistic>
+          <Card.Content>
+            <Button color="red" fluid onClick={this.reset}>
+              <Icon name="redo" />
+              Reset
+            </Button>
+          </Card.Content>
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default App;
