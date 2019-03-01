26d9b86c2f5281bf97f2b79b1f81fe08af7a0ca0
Yuka O'oka <oukayuka@ayeate.com>
2019-03-01 11:49:25 +0900

    Add chapter 7-5

---
+++ 07-component/05-functional/src/App.tsx
@@ -0,0 +1,37 @@
+import React, { FC } from 'react';
+import CharacterList, { Character } from './CharacterList';
+
+import './App.css';
+
+const App: FC<{}> = () => {
+  const characters: Character[] = [
+    {
+      id: 1,
+      name: '羽咲 綾乃',
+      age: 16,
+      height: 151,
+    },
+    {
+      id: 2,
+      name: '荒垣 なぎさ',
+      age: 18,
+      height: 174,
+    },
+    {
+      id: 3,
+      name: '泉 理子',
+      age: 18,
+    },
+  ];
+
+  return (
+    <div className="container">
+      <header>
+        <h1>はねバド！ キャラクター一覧</h1>
+      </header>
+      <CharacterList school="北小町高校" characters={characters} />
+    </div>
+  );
+};
+
+export default App;
