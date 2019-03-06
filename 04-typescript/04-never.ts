335127eff237e70ccfd910a4c6696b3496f06fca
Yuka O'oka <oukayuka@ayeate.com>
2019-03-06 11:48:43 +0900

    Add never type into chapter 4

---
+++ 04-typescript/04-never.ts
@@ -0,0 +1,14 @@
+const greet = (friend: 'serval' | 'caracal' | 'cheetah') => {
+  switch (friend) {
+    case 'serval':
+      return 'Hello, Serval!';
+    case 'caracal':
+      return 'Hello, Caracal!';
+    case 'cheetah':
+      return 'Hello, Cheetah!';
+    default:
+      const check: never = friend;
+  }
+};
+
+console.log(greet('serval'));
