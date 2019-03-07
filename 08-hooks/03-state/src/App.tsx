2b2a08bc5a70dd08f509b9c4a955e8bf30d5cf84
Yuka O'oka <oukayuka@ayeate.com>
2019-03-07 18:37:03 +0900

    Add chapter 8-3

---
+++ 08-hooks/03-state/src/App.tsx
@@ -0,0 +1,42 @@
+import React, { FC, useState } from 'react';
+import { Button, Card, Statistic } from 'semantic-ui-react';
+
+import './App.css';
+
+const App: FC = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div className="container">
+      <header>
+        <h1>カウンター</h1>
+      </header>
+      <Card>
+        <Statistic className="number-board">
+          <Statistic.Label>count</Statistic.Label>
+          <Statistic.Value>{count}</Statistic.Value>
+        </Statistic>
+        <Card.Content>
+          <div className="ui two buttons">
+            <Button color="red" onClick={decrement}>
+              -1
+            </Button>
+            <Button color="green" onClick={increment}>
+              +1
+            </Button>
+          </div>
+        </Card.Content>
+      </Card>
+    </div>
+  );
+};
+
+export default App;
