0fef4e988f5d6155030b90116d6bcc84d0889184
Yuka O'oka <oukayuka@ayeate.com>
2019-03-14 10:07:13 +0900

    Modify 3-5 Clusure sample code

--- 03-fp/05-closure.js
+++ 03-fp/05-closure.js
@@ -1,10 +1,10 @@
-const counter = () => {
-  let c = 0;
-  const increment = () => ++c;
+const counterMaker = (initialCount = 0) => {
+  let c = initialCount;
+  const increment = () => c++;
 
   return increment;
 };
 
-const count = counter();
+const count = counterMaker(1);
 console.log(count(), count(), count());  // 1 2 3
 
