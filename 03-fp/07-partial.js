dbbdd5fe6a8f855a43cbaacb04a0313143e35b7a
Yuka O'oka <oukayuka@ayeate.com>
2020-01-25 08:49:40 +0900

    fix sample code 3-7

--- 03-fp/07-partial.js
+++ 03-fp/07-partial.js
@@ -1,6 +1,6 @@
 const multi = n => m => n * m;
-console.log(multi(3)(15));    // 15
+console.log(multi(3)(5));   // 15
 
 const triple = multi(3);
-console.log(triple(5));       // 15
+console.log(triple(5));     // 15
 
